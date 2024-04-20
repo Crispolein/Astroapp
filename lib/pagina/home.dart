9c4d7c56e9a10b7763dc4dffad96a8abb0eb7115
CrisElOlvidadiso <103906608+CrisElOlvidadiso@users.noreply.github.com>
2024-04-20 11:44:07 -0400

    cambios al home
    cambios al home, importacion de curved_navigation_bar: ^1.0.3 y 2 imagenes

--- lib/pagina/home.dart
+++ lib/pagina/home.dart
@@ -1,9 +1,11 @@
 import 'package:astro_app/pagina/review_quiz_page.dart';
 import 'package:astro_app/pagina/quiz_page.dart';
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,42 +15,217 @@ class HomePage extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomeScreen(),
-      debugShowCheckedModeBanner: false, // Aquí se oculta la etiqueta de debug
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
+      body: Stack(
+        children: [
+          if (_selectedIndex == 2)
+            _buildBackgroundImage(), // Solo muestra la imagen de fondo en la sección de "Juegos"
+          _buildContentView(),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const QuizPage()));
-                // Aquí puedes agregar la lógica para iniciar el quiz
-              },
-              child: Text('Iniciar Quiz'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const ReviewQuizPage()));
-              },
-              child: Text('Repasar Quiz'),
-            ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 134, 160, 255),
+              Color.fromARGB(255, 255, 0, 0),
+              Color.fromARGB(255, 0, 255, 0),
+              Color.fromARGB(255, 0, 0, 255),
+              Color.fromARGB(255, 255, 255, 0),
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+        ),
+        child: CurvedNavigationBar(
+          color: Colors.blue,
+          backgroundColor: Colors.transparent,
+          items: <Widget>[
+            Icon(Icons.account_circle, size: 30),
+            Icon(Icons.info, size: 30),
+            Icon(Icons.sports_esports, size: 30),
           ],
+          onTap: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
         ),
       ),
     );
+  }
+
+  Widget _buildBackgroundImage() {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/imagen4.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildContentView() {
+    if (_selectedIndex == 2) {
+      // Solo muestra los botones en la sección de "Juegos"
+      return Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Container(
+            width:
+                MediaQuery.of(context).size.width * 0.9, // Ancho del container
+            child: GridView.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 70,
+              crossAxisSpacing: 30,
+              childAspectRatio:
+                  1.2, // Relación de aspecto para controlar el tamaño de los cuadrados
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const QuizPage()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red, // Cambia el color del botón
+                    padding: EdgeInsets.all(
+                        20), // Ajusta el espacio dentro del botón
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text(
+                    'Iniciar Quiz',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ReviewQuizPage()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green, // Cambia el color del botón
+                    padding: EdgeInsets.all(
+                        20), // Ajusta el espacio dentro del botón
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text(
+                    'Repasar Quiz',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ReviewQuizPage()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue, // Cambia el color del botón
+                    padding: EdgeInsets.all(
+                        20), // Ajusta el espacio dentro del botón
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text(
+                    'juego 3',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ReviewQuizPage()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.yellow, // Cambia el color del botón
+                    padding: EdgeInsets.all(
+                        20), // Ajusta el espacio dentro del botón
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text(
+                    'juego 4',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ReviewQuizPage()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(
+                        255, 32, 241, 227), // Cambia el color del botón
+                    padding: EdgeInsets.all(
+                        20), // Ajusta el espacio dentro del botón
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text(
+                    'juego 5',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const ReviewQuizPage()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(
+                        255, 255, 49, 231), // Cambia el color del botón
+                    padding: EdgeInsets.all(
+                        20), // Ajusta el espacio dentro del botón
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text(
+                    'juego 6',
+                    style: TextStyle(fontSize: 16, color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    } else {
+      return Container(); // Oculta el contenido en las secciones diferentes a "Juegos"
+    }
   }
 }
