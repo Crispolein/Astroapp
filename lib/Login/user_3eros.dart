bb707478d69a0e5ee2fb51678bd85a4ab0a5dbc7
Christian Elias Rangel Intriago <102484053+Crispolein@users.noreply.github.com>
2024-06-07 20:34:49 -0400

    Borrando librerias innecesarias

--- lib/Login/user_3eros.dart
+++ lib/Login/user_3eros.dart
@@ -1,4 +1,3 @@
-import 'package:astro_app/models/proyecto_model.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:astro_app/vistausuario2/homeb.dart';
